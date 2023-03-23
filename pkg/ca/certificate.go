0727c9b58fac56ba34695c10dcff1816ca7c20bc
chentanjun <2799194073@qq.com>
2023-03-23 18:55:59 +0800

    aeraki support dubbo example

--- pkg/ca/certificate.go
+++ pkg/ca/certificate.go
@@ -37,8 +37,8 @@ type KeyCertBundle struct {
 }
 
 // GenerateKeyCertBundle generates root ca and server certificate
-func GenerateKeyCertBundle(client kubelib.Client) (*KeyCertBundle, error) {
-	caKeyCertBundle, err := getIstioCA(client.CoreV1())
+func GenerateKeyCertBundle(client kubelib.Client, namespace string) (*KeyCertBundle, error) {
+	caKeyCertBundle, err := getIstioCA(client.CoreV1(), namespace)
 	if err != nil {
 		return nil, err
 	}
